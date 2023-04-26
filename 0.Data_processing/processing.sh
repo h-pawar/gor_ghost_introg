6e94490f11bd26ebabc96c668899f011dbda42d8
kuhlwilm <79041549+kuhlwilm@users.noreply.github.com>
2023-04-26 13:57:27 +0200

    Update processing.sh

--- 0.Data_processing/processing.sh
+++ 0.Data_processing/processing.sh
@@ -26,4 +26,31 @@ spec="gorilla"
 # for each chromosome $chrom
 zcat /scratch/devel/mkuhlwilm/gvcfs/greatapeN_"$chrom".vcf.gz | sed -e 's/;MLEAC=.*;MLEAF=.*;/;/g' | /apps/BCFTOOLS/1.9/bin/bcftools view -S $fil -a -U -V indels - | /apps/BCFTOOLS/1.9/bin/bcftools view -m 2 - | /apps/BCFTOOLS/1.9/bin/bcftools filter --exclude 'GT~"\."' | /apps/BCFTOOLS/1.9/bin/bcftools filter --include '(MIN(FMT/DP) >= 6) && (MAX(FMT/DP) <= 100) && (MIN(FMT/MQ) > 20)' | /apps/BCFTOOLS/1.9/bin/bcftools filter --include '((FMT/MQ0)/(FMT/DP)) < 0.1' | intersectBed -wa -v -header -a stdin -b /scratch/project/devel/mkuhlwilm/RM.bed | intersectBed -wa -v -header -a - -b <(zcat /scratch/devel/mkuhlwilm/arch/filter/Na_"$spec"_"$chrom".bed.gz ) | /scratch/devel/mkuhlwilm/jvarkit/jvarkit/dist/vcfbigwig -T mapability -B /scratch/project/devel/mkuhlwilm/wgEncodeDukeMapabilityUniqueness35bp.bigWig /dev/stdin/ | egrep "#|mapability=1" | /apps/BCFTOOLS/1.9/bin/bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t.\tGT[\t%GT]\n' - | sed 's/\//|/g' | cat /scratch/devel/mkuhlwilm/arch/subsets/gehdr.txt - | bgzip -f > /scratch/devel/mkuhlwilm/arch/subsets/3_"$spec"_"$chrom".vcf.gz
 
-## step 5: filtering for 
+## step 5: filtering for hmmix/Skov method
+## get weights files using an R script
+module load java/1.8.0u31 TABIX/0.2.6 gcc/6.3.0 xz python/2.7.11 R/3.2.0 perl BCFTOOLS/1.6 SAMTOOLS/1.0 BEDTools/2.26.0 bedops vcftools
+R CMD BATCH --vanilla --slave "--args ${chrom}" /home/devel/mkuhlwilm/programs/get_clbl.R
+
+## get mutation rate files 
+module load java/1.8.0u31 TABIX/0.2.6 gcc/6.3.0 xz python/2.7.11 R/3.2.0 perl BCFTOOLS/1.6 SAMTOOLS/1.0 BEDTools/2.26.0 bedops vcftools
+/apps/BCFTOOLS/1.9/bin/bcftools view -S /scratch/devel/mkuhlwilm/arch/"$spec".lst /scratch/devel/mkuhlwilm/gvcfs/greatapeN_"$chr".vcf.gz -G | intersectBed -wa -v -header -a stdin -b /project/devel/mkuhlwilm/RM.bed | /scratch/devel/mkuhlwilm/jvarkit/jvarkit/dist/vcfbigwig -T mapability -B /project/devel/mkuhlwilm/wgEncodeDukeMapabilityUniqueness35bp.bigWig /dev/stdin/ | egrep "#|mapability=1" | bedtools merge -i stdin > /scratch/devel/mkuhlwilm/arch/mura/chr_"$chr"mura_"$spec".bed
+/apps/BCFTOOLS/1.9/bin/bcftools view -m2 -M2 -S /scratch/devel/mkuhlwilm/arch/"$spec".lst /scratch/devel/mkuhlwilm/gvcfs/greatapeN_"$chr".vcf.gz | intersectBed -wa -header -a stdin -b /scratch/devel/mkuhlwilm/arch/mura/chr_"$chr"mura_"$spec".bed | vcftools --vcf - --counts --stdout --remove-indels > /dev/shm/mydata/"$chr"_"$spec".freq
+awk -v c="$chr" '$1==c' /scratch/devel/mkuhlwilm/arch/N2_"$spec"_weights_float.txt > /dev/shm/mydata/"$chr"_"$spec"_weight.txt
+python /home/devel/mkuhlwilm/programs/skov/Introgression-detection-master4/Estimate_mutationrate.py /dev/shm/mydata/"$chr"_"$spec".freq 100000 1000 /dev/shm/mydata/"$chr"_"$spec"_weight.txt /scratch/devel/mkuhlwilm/arch/mura/chr_"$chr"mura_"$spec".mut
+
+spec=$(echo $spec | cut -f 6 -d "/" | cut -f 1 -d ".")
+sed -e 's/0\.0/0\.1/g' /scratch/devel/mkuhlwilm/arch/mura/chr_1mura_"$spec".mut > /scratch/devel/mkuhlwilm/arch/mura/mura_"$spec".mut
+for chrom in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X
+  do echo $chrom
+  sed -e 's/0\.0/0\.1/g' /scratch/devel/mkuhlwilm/arch/mura/chr_"$chrom"mura_"$spec".mut >> /scratch/devel/mkuhlwilm/arch/mura/mura_"$spec".mut
+  done
+
+## get observations file using an R script
+module load TABIX/0.2.6 gcc/6.3.0 xz python/2.7.11 R/3.2.0 perl BCFTOOLS/1.6 SAMTOOLS/1.0 BEDTools/2.26.0 bedops vcftools
+# per chromosome
+R CMD BATCH --vanilla --slave "--args ${chrom}" /home/devel/mkuhlwilm/programs/getgeno1.R
+
+
+R CMD BATCH --vanilla --slave "--args ${chrom}" /home/devel/mkuhlwilm/programs/getgeno2.R
+
+R CMD BATCH --vanilla --slave "--args ${chrom}" /home/devel/mkuhlwilm/programs/getgeno.R
