a52ac01abc8722b107de3284bf8cec220ab0afbd
h-pawar <68990937+h-pawar@users.noreply.github.com>
2023-04-28 17:37:59 +0200

    Update archaicmb.recovered.R

--- 6.Introg_regions/6.1.overlap.sstar.skov/scripts/archaicmb.recovered.R
+++ 6.Introg_regions/6.1.overlap.sstar.skov/scripts/archaicmb.recovered.R
@@ -6,7 +6,6 @@ options(stringsAsFactors=F)
 library(tidyverse)
 options(scipen=100)
 library(adegenet)
-#library(mgcv)
 library(GenomicRanges)
 load(file="/scratch/devel/hpawar/admix/overlap.s*.skov/overlap_objects/ov_gbb_99",verbose=T)
 load(file="/scratch/devel/hpawar/admix/overlap.s*.skov/overlap_objects/ov_gbg_99",verbose=T)
@@ -15,20 +14,18 @@ gg<-c(ov_gbg_99[[1]],ov_gbg_99[[2]],ov_gbg_99[[3]],ov_gbg_99[[4]],ov_gbg_99[[5]]
 gb<-c(ov_gbb_99[[1]],ov_gbb_99[[2]],ov_gbb_99[[3]],ov_gbb_99[[4]],ov_gbb_99[[5]],ov_gbb_99[[6]],ov_gbb_99[[7]],ov_gbb_99[[8]],ov_gbb_99[[9]],ov_gbb_99[[10]],ov_gbb_99[[11]],ov_gbb_99[[12]])
 
 sum(reduce(gg)@ranges@width)/1000000
-[1] 115.1618
+#[1] 115.1618
 # ~115 Mbp in GBG
 sum(reduce(gb)@ranges@width)/1000000
-[1] 152.5841
+#[1] 152.5841
 # ~153 Mbp in GBB
 sum(reduce(c(gb,gg))@ranges@width)/1000000
-[1] 213.9295
+#[1] 213.9295
 # ~214 Mbp
-# just to approximate:
-#sum(reduce(c(gb,gg))@ranges@width)/3300000000*100
-# ~6.5% of the archaic genome is recovered
+
 
 sum(reduce(c(gb,gg))@ranges@width)/2881033286*100
-[1] 7.425443
+#[1] 7.425443
 
 # for action of the genome (minus regions without data) 
 # 1) read in the informative regions (ie with callable sites)
@@ -41,14 +38,9 @@ informative[,1]<-sapply("chr",paste, informative[,1], sep="")
 informranges<-GRanges(seqnames=informative[,1],ranges=IRanges(start=as.numeric(informative[,2]),end=as.numeric(informative[,2]+40000),names=informative[,1]),strand=rep("*",length(informative[,1])),prop=as.numeric(informative[,3]))
 
 sum(reduce(informranges)@ranges@width)/1000000
-[1] 1302.452
 
 
-> sum(reduce(gg)@ranges@width)/sum(reduce(informranges)@ranges@width)
-[1] 0.08841923
->
-> sum(reduce(gb)@ranges@width)/sum(reduce(informranges)@ranges@width)
-[1] 0.1171515
->
-> sum(reduce(c(gb,gg))@ranges@width)/sum(reduce(informranges)@ranges@width)
-[1] 0.1642514
+
+sum(reduce(gg)@ranges@width)/sum(reduce(informranges)@ranges@width)
+sum(reduce(gb)@ranges@width)/sum(reduce(informranges)@ranges@width)
+sum(reduce(c(gb,gg))@ranges@width)/sum(reduce(informranges)@ranges@width)
