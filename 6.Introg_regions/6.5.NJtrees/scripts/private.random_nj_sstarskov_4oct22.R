8914eaac12bda7b9d043e71f6fbb42b9b9bf910d
h-pawar <68990937+h-pawar@users.noreply.github.com>
2023-05-02 10:26:02 +0200

    Update private.random_nj_sstarskov_4oct22.R

--- 6.Introg_regions/6.5.NJtrees/scripts/private.random_nj_sstarskov_4oct22.R
+++ 6.Introg_regions/6.5.NJtrees/scripts/private.random_nj_sstarskov_4oct22.R
@@ -1,9 +1,6 @@
 # Tue  4 Oct 2022 10:19:26 CEST
 # generate random nj trees of private regions (intersect sstar-skov regions which are found only in one id of the pop)
 
-# ls -lhtr /scratch/devel/hpawar/admix/overlap.s*.skov/privateregionsperid/random/GBB/nj
-#total 0
-
 #-----------------------------------------------------------------------------------------------------------------------
 
 # module load gcc/6.3.0 openssl/1.0.2q  R/4.0.1  BCFTOOLS/1.14
@@ -13,11 +10,7 @@ options(stringsAsFactors=F)
 library(tidyverse)
 options(scipen=100)
 library(adegenet)
-#library(mgcv)
 library(GenomicRanges)
-#BiocManager::install(c("GenomicRanges", "plyranges", "HelloRangesData"))
-#library(plyranges) # may not be necessary here
-#library(GenomicFeatures) # if need to read in the gtf
 library(tidyr)
 library(phangorn) 
 library(ape) 
@@ -35,33 +28,21 @@ spec=args[3]
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
 
-#bed file -> intersect with vcf, extract biallelic snps only -> output chr, pos, gts
-
 random_intersect_introgbed_regions<-function(ind,SPE,spe,chrom){
 a=ind
-#/scratch/devel/hpawar/admix/overlap.s*.skov/privateregionsperid/random/GBB
 tmp<-paste("/scratch/devel/hpawar/admix/overlap.s*.skov/regionsperid/random/",SPE,"/",spe,".",a,".random.tmp.bed",sep="")
 
 chrom=chrom
-# filter by biallelic snps only, query by the putative introg regions for id 1 & output chr, pos & gts & ref & alt alleles
 testsnps<-system(paste("bcftools view -m2 -M2 -v snps -R ",tmp," /scratch/devel/mkuhlwilm/arch/subsets/3_gorilla_",chrom,".vcf.gz | bcftools query -f '%CHROM %POS %REF %ALT [%GT ]\n' ",sep=""),intern=T) 
-testsnps<-do.call(rbind,strsplit(testsnps,split=" "))  ## ie list merged vertically into df
+testsnps<-do.call(rbind,strsplit(testsnps,split=" "))
 
 return(testsnps)
 }
 
 #-----------------------------------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------------------------------------
 
-# AMEND BELOW ***
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# samples & their populations
-# for ids
 samples.in.vcf<-system(paste("bcftools query -l /scratch/devel/mkuhlwilm/arch/subsets/3_gorilla_22.vcf.gz",sep=""),intern=T) # -l, --list-samples: list sample names and exit
 
-# add population identifier
 identifiers<-cbind(samples.in.vcf, c(rep("GBB",12), rep("GBG",9), rep("GGD",1),rep("GGG",27)))
 colnames(identifiers)<-c("ids","pop")
 #-----------------------------------------------------------------------------------------------------------------------
@@ -69,10 +50,6 @@ colnames(identifiers)<-c("ids","pop")
 
 random_test_gts_topca<-function(ind,SPE,spe) {
 
-#random_granges_tobed(scen,ind,SPE,spe)
-
-# extract biallelic GTs for introg regions across all autosomes for the first individual
-# loop over all chr - to obtain testsnps df for all chr
 autosome_sk_oneid<-list()
 for (chr in (1:22)) {  
 autosome_sk_oneid[[chr]]<-random_intersect_introgbed_regions(ind,SPE,spe,chr)
@@ -92,34 +69,20 @@ return(aut_oneid)
 
 
 generate_tree_perid<-function(ind,SPE,spe) {
-# run through for 1 individual first
-#sbset<-test_gts_tonj(sk_regions_gbb,1,"GBB","gbb") # if is fine, then can amend to run for all individuals # yes works
 
 sbset<-random_test_gts_topca(ind,SPE,spe)
-# MK:
-#2) maybe the reason why K80 didnt work was that you ended up 
-#with monomorphic sites in your dataset, I suggest to remove these:
+
 ssb<-matrix(as.numeric(sbset[,-c(1:4)]),ncol=ncol(sbset)-4)
-#head(ssb)
-#     [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10] [,11] [,12] [,13] [,14]
-#[1,]    1    1    1    1    1    1    1    1    1     1     1     1     1     1
-#[2,]    1    1    1    0    0    0    1    1    1     1     1     1     0     0
+
 sr<-rowSums(ssb)
 r2<-which(sr==0)
-sbset2<-sbset[-r2,] # remove sites where all ids are homozy for the ref allele
-
-#3) You may want to retain only a subset of the Homo-Gorilla differences, 
-#as it is just for rooting and there are plenty of such sites:
+sbset2<-sbset[-r2,] 
+ 
 r1<-which(sr[-r2]==49) # sample sites where all gor ids are fixed derived (relative to the human ref)
 sbset2<-sbset2[-sample(r1,size=round(length(r1)*.9)),]
-#head(sbset2)
-#     [,1]   [,2]      [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10] [,11] [,12]
-#[1,] "chr1" "5200698" "T"  "C"  "1"  "1"  "1"  "0"  "0"  "0"   "1"   "1"  
-#[2,] "chr1" "5200750" "G"  "T"  "1"  "1"  "1"  "0"  "0"  "0"   "1"   "0"  
-#[3,] "chr1" "5200756" "T"  "C"  "0"  "0"  "0"  "1"  "0"  "1"   "1"   "1"  
  
 #-----------------------------------------------------------------------------------------------------------------------
-# numeric GTs -> letters
+
 hold_rows<-list()
 for (i in (1:nrow(sbset2))) { 
 tes<-sbset2[i,]
@@ -134,67 +97,31 @@ hold_rows[[i]]<-tes
 
 gts<-do.call(rbind,hold_rows)
 
-#> head(gts)
-#     [,1]   [,2]      [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10] [,11] [,12]
-#[1,] "chr1" "5200698" "T"  "C"  "C"  "C"  "C"  "T"  "T"  "T"   "C"   "C"  
-#[2,] "chr1" "5200750" "G"  "T"  "T"  "T"  "T"  "G"  "G"  "G"   "T"   "G"  
-
 gts_2<-t(gts[,c(3,5:ncol(gts))])
-#str(gts_2)
-# chr [1:50, 1:383816] "A" "T" "T" "T" "T" "T" "T" "T" "T" "T" "T" "T" "T" ...
 
 sample<-c('Homo_sapiens',samples.in.vcf)
 rownames(gts_2)<-sample
 
 db_2<-as.DNAbin(gts_2)
 
-# MK
-#4) Now, the dm works with K80:
-#> dm_2 = dist.dna(db_2,pairwise.deletion=T,model="K80")
-
 dm_2 = dist.dna(db_2,pairwise.deletion=F,model="K80")
-
-# head(dm_2)
-#[1] 0.4978397 0.4821156 0.4840672 0.4907302 0.4880527 0.4841037
 
 outg<-c('Homo_sapiens')
 
 treeNJ = NJ(dm_2);  fit = pml(treeNJ, data=phyDat(gts_2)); fitJC = optim.pml(fit, TRUE)
-#treeNJ
-#Phylogenetic tree with 50 tips and 48 internal nodes.
 
 #-----------------------------------------------------------------------------------------------------------------------
 rootedNJ <- ladderize(root(treeNJ, outgroup=outg, resolve.root=TRUE))
 #-----------------------------------------------------------------------------------------------------------------------
-#rootedNJ
-#Phylogenetic tree with 50 tips and 49 internal nodes.
-#-----------------------------------------------------------------------------------------------------------------------
-
-# alternate way of doing the bootstrapping
-#boots <- boot.phylo(phy=rootedNJ,
-#                    x=db_2,FUN=function(bs_db_2) nj(dist.dna(bs_db_2, model="K80")),
-#                    trees=TRUE)
-
-# Add bootstraps to PHYLO object
-#rootedNJ$node.label<-boots
-
-# ls -lhtr /scratch/devel/hpawar/admix/overlap.s*.skov/privateregionsperid/random/GBB/nj
 
 save(rootedNJ, file=paste("/scratch/devel/hpawar/admix/overlap.s*.skov/privateregionsperid/random/",SPE,"/nj/",spe,".id.",ind,".private.random.rootedNJploutg.Robj.tree",sep=""))
 
-
-#bss = bootstrap.pml(fitJC, bs=100, optNni=TRUE, control = pml.control(trace = 0)) # this step was causing recursive errors in the cluster
-
-# drop the outgroup before doing the bootstrapping
 rootedNJ <- drop.tip(rootedNJ, "Homo_sapiens")
-#bss = bootstrap.pml(fitJC, bs=10, optNni=TRUE, control = pml.control(trace = 0)) # this works
-bss = bootstrap.pml(fitJC, bs=100, optNni=TRUE, control = pml.control(trace = 0)) # generate 100 bs replicates instead of 10
+bss = bootstrap.pml(fitJC, bs=100, optNni=TRUE, control = pml.control(trace = 0))
 
 random_out<-list(rootedNJ,bss)
 
 save(random_out, file=paste("/scratch/devel/hpawar/admix/overlap.s*.skov/privateregionsperid/random/",SPE,"/nj/",spe,".id.",ind,".private.random.Robj.tree",sep=""))
-
-#write.tree(rootedNJ, file=paste("/scratch/devel/hpawar/admix/overlap.s*.skov/trees/random/",SPE,"/",spe,".id.",ind,".random.tree",sep="")  )
 
 }
 
